ffb75d85a5bada3a64b7bd2a52affce61c615509
dtmesa <dylantmesa@gmail.com>
2026-07-02 18:31:44 -0700

    debug

--- src/redis/client.redis.ts
+++ src/redis/client.redis.ts
@@ -1,10 +1,13 @@
 import { Redis } from "ioredis";
 
 export const redis = new Redis(process.env.REDIS_URL ?? "redis://localhost:6379", {
-	lazyConnect: false,
 	connectTimeout: 5000,
 	maxRetriesPerRequest: 1,
+	enableReadyCheck: true,
+	lazyConnect: false,
 });
+
+console.log(process.env.REDIS_URL);
 
 redis.on("connect", () => console.log("Redis connecting"));
 redis.on("ready", () => console.log("Redis ready"));
