8f2c7706b0359a64a4580ea91382c2b7c592c45f
dtmesa <dylantmesa@gmail.com>
2026-07-02 18:29:31 -0700

    debug

--- src/redis/client.redis.ts
+++ src/redis/client.redis.ts
@@ -5,3 +5,8 @@ export const redis = new Redis(process.env.REDIS_URL ?? "redis://localhost:6379"
 	connectTimeout: 5000,
 	maxRetriesPerRequest: 1,
 });
+
+redis.on("connect", () => console.log("Redis connecting"));
+redis.on("ready", () => console.log("Redis ready"));
+redis.on("error", (err) => console.log("Redis error", err));
+redis.on("close", () => console.log("Redis closed"));
